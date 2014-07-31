50f2f6511bb8752bb49a3842764b6d1f4371ff62
Anthony Martinet <anthony@alkivi.fr>
2014-07-31 17:11:21 +0200

    fix params name to handle puppet upgrade

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,22 @@
-class alkivi_backup::config () {
+class alkivi_backup::config (
+    $encryption        = $alkivi_backup::encryption,
+    $passphrase        = $alkivi_backup::passphrase,
+    $root              = $alkivi_backup::root,
+    $dest              = $alkivi_backup::dest,
+    $include_dir       = $alkivi_backup::include_dir,
+    $exclude_dir       = $alkivi_backup::exclude_dir,
+    $static_options    = $alkivi_backup::static_options,
+    $clean_up_type     = $alkivi_backup::clean_up_type,
+    $clean_up_variable = $alkivi_backup::clean_up_variable,
+    $logdir            = $alkivi_backup::logdir,
+    $log_file          = $alkivi_backup::log_file,
+    $log_owner         = $alkivi_backup::log_owner,
+    $verbosity         = $alkivi_backup::verbosity,
+    $email             = $alkivi_backup::email,
+    $email_from        = $alkivi_backup::email_from,
+    $email_subject     = $alkivi_backup::email_subject,
+    $mail_command      = $alkivi_backup::mail_command,
+) {
   File {
     ensure  => present,
     owner   => 'root',
